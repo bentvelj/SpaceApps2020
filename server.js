a7a470dfa0796fc804919a37def0caa0f43d991e
Matthew McCracken <mccracm@mcmaster.ca>
2020-10-03 11:54:46 -0400

    cleared app.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+let express = require('express');
 const app = express();
+let bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -8,4 +11,4 @@ const port = process.env.PORT || 4000; //setting port for dev or production
 
 const server = app.listen(port, () => {
     console.log('Connected to port ' + port)
-})
+});
