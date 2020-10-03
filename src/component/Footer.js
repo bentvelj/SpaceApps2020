a0bf451cff36aef67a063ab1b751ae71173c3db4
Christopher Andrade <christopherandrade716@gmail.com>
2020-10-03 18:23:48 -0400

    Added links to Footer component.

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import git_logo from '../res/git_logo.png';
+import challenge_logo from '../res/challenge_logo.png';
+import dataset_logo from '../res/dataset_logo.png';
 
 class Footer extends Component
 {
@@ -6,6 +9,7 @@ class Footer extends Component
     {
         return {
             display: "flex",
+            justifyContent: "space-around",
             backgroundColor: "#3A3A3A",
             width: "100%",
             height: "200px",
@@ -25,7 +29,9 @@ class Footer extends Component
     {
         return {
             textDecoration: "none",
-            color: "#FFFFFF"
+            display: "flex",
+            width: "100px",
+            right: "100px"
         }
     };
 
@@ -37,19 +43,39 @@ class Footer extends Component
         }
     };
 
+    image_style = () =>
+    {
+        return {
+            padding: "5px",
+            width: "30%"
+        }
+    }
+
     render()
     {
         return(
-            <div className="total" style={this.total_style()}>
-                <div className="column" style={this.column_style()}>
-                    <h5 className="text" style={this.text_style()}></h5>
-                    <h5 className="text" style={this.text_style()}>Made for NASA SpaceApps Hackathon 2020</h5>
-                    <h5 className="text" style={this.text_style()}></h5>
+            <div style={this.total_style()}>
+                <div style={this.column_style()}>
+                    <h5 style={this.text_style()}></h5>
+                    <h5 style={this.text_style()}>Made for NASA SpaceApps Hackathon 2020</h5>
+                    <h5 style={this.text_style()}></h5>
                 </div>
-                <div className="column" style={this.column_style()}>
-                    <a href="https://github.com/bentvelj/SpaceApps2020" className="link" style={this.link_style()}>Github</a>
-                    <a href="https://2020.spaceappschallenge.org/challenges/connect/space-exploration-your-backyard/details" className="link" style={this.link_style()}>NASA Challenge</a>
-                    <a href="" className="link" style={this.link_style()}>Dataset</a>
+
+                <div style={this.column_style()}>
+                    <a style={this.link_style()} href="https://github.com/bentvelj/SpaceApps2020">
+                        <h5 style={this.text_style()}>Github</h5>
+                        <img src={git_logo} style={this.image_style()}></img>
+                    </a>
+
+                    <a style={this.link_style()} href="https://2020.spaceappschallenge.org/challenges/connect/space-exploration-your-backyard/details">
+                        <h5 style={this.text_style()}>Challenge</h5>
+                        <img src={challenge_logo} style={this.image_style()}></img>
+                    </a>
+
+                    <a style={this.link_style()} href="">
+                        <h5 style={this.text_style()}>Dataset</h5>
+                        <img src={dataset_logo} style={this.image_style()}></img>
+                    </a>
                 </div>
             </div>
         )
