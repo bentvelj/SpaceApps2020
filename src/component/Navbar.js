a09fa7ecaa219295d53e1c0c6b5721e7fbe60847
Matthew McCracken <mccracm@mcmaster.ca>
2020-10-03 14:18:22 -0400

    push

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
-import logo from '../res/LogoFinal.png';
+import logo from '../res/FinalLogo.png';
 
 class Navbar extends React.Component {
     render() {
         return (
             <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
                 <div className="container">
-                    <img src={logo} alt="SpaceNearby Logo" href="#" style="width:"></img>
+                    <img src={logo} alt="SpaceNearby Logo" href="#"></img>
                     <div className="collapse navbar-collapse">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
