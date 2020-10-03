c8434df42296ba1a1f936ca112a2d4e89eb1a752
Matthew McCracken <mccracm@mcmaster.ca>
2020-10-03 17:19:26 -0400

    fixed class name error

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,19 +5,19 @@ import logo from '../res/FinalLogo.png';
 class Navbar extends React.Component {
     render() {
         return (
-            <nav className="navbar navbar-expand navbar-light mb-4" style={{backgroundImage: "linear-gradient(120deg, #1cc4df, #c908a7)"}}>
+            <nav className="navbar navbar-expand navbar-light mb-4" style={{ backgroundImage: "linear-gradient(120deg, #1cc4df, #c908a7)" }}>
                 <div className="container">
-                    <img src={logo} alt="SpaceNearby Logo" href="#" style={{width:'50%'}}></img>
+                    <img src={logo} alt="SpaceNearby Logo" href="#" style={{ width: '50%' }}></img>
                     <div className="collapse navbar-collapse">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
-                                <button type="button" class="btn btn-primary mx-4" href="/">About</button>
+                                <button type="button" className="btn btn-primary mx-4" href="/">About</button>
                             </li>
                             <li className="nav-item">
-                            <button type="button" class="btn btn-primary mx-4" href="/search">Search</button>
+                                <button type="button" className="btn btn-primary mx-4" href="/search">Search</button>
                             </li>
                             <li className="nav-item">
-                            <button type="button" class="btn btn-primary mx-4" href="/whatshot">What's Hot</button>
+                                <button type="button" className="btn btn-primary mx-4" href="/whatshot">What's Hot</button>
                             </li>
                         </ul>
                     </div>
