286de9b09ed565e37b510501acec7e8ecbfe4d35
Yuvraj Randhawa <randhawy@mcmaster.ca>
2020-10-03 19:05:22 -0400

    Blue Gradient Navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,7 +5,7 @@ import logo from '../res/FinalLogo.png';
 class Navbar extends React.Component {
     render() {
         return (
-            <nav className="navbar navbar-expand navbar-light mb-4" style={{backgroundImage: "linear-gradient(120deg, #a30cd0, #940db9)"}}>
+            <nav className="navbar navbar-expand navbar-light mb-4" style={{backgroundImage: "linear-gradient(120deg, #0c6cd0, #0d64b9)"}}>
                 <div className="container">
                     <div className='d-flex justify-content-center'>
                         <img className='m-4' src={logo} alt="SpaceNearby Logo" href="#" style={{position:'relative', left:'15%', width:'35%'}}></img>
