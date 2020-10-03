7e6125310afaf0d76b9356e107c6a11c25dde803
Matthew McCracken <mccracm@mcmaster.ca>
2020-10-03 16:54:08 -0400

    finished server side rendering fetch first route

--- src/component/WhatsHot.js
+++ src/component/WhatsHot.js
@@ -12,13 +12,13 @@ class WhatsHot extends React.Component {
     componentDidMount() {
         // var header = new Headers();
         // header.append("api_key", '6NtHJvHpr8mralq5YlKnn9664eVXHxw1wfZrgyBA')
-        var request = {
-            method: 'GET',
-            // headers: header
-        }
-        console.log(request)
+        // var request = {
+        //     method: 'GET',
+        //     // headers: header
+        // }
+        // console.log(request)
 
-        fetch(`https://localhost:4000/pic`, request)
+        fetch(`http://localhost:4000/pic`)
             .then(res => res.json())
             .then(data => this.setState({ pic: data }))
 
@@ -29,9 +29,9 @@ class WhatsHot extends React.Component {
         return (
             <div>
                 <Layout>
-                    <img src={this.state.pic.hdurl} alt="hotimage"></img>
+                    <img src={this.state.pic.hdurl} alt="hotimage" style={{ width: "40vw" }}></img>
                 </Layout>
-            </div>
+            </div >
         )
 
     }
--- user_routes/routes.js
+++ user_routes/routes.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const router = express.Router();
-
+const fetch = require("node-fetch");
+global.Headers = fetch.Headers;
 
 router.get("/pic", (req, res) => {
     console.log("thing")
 
     var request = {
         method: 'GET',
-        headers: { "api_key": '6NtHJvHpr8mralq5YlKnn9664eVXHxw1wfZrgyBA' }
+        headers: new Headers({
+            'api_key': '6NtHJvHpr8mralq5YlKnn9664eVXHxw1wfZrgyBA'
+        })
     }
 
 
-    fetch(`https://api.nasa.gov/planetary/apod`, request)
-        .then(data => res.send(data))
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=6NtHJvHpr8mralq5YlKnn9664eVXHxw1wfZrgyBA`, request)
+        .then(data => data.json())
+        .then(rsp => res.send(rsp))
 
 });
 
