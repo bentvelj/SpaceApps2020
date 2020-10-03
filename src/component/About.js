4e5570e4c069b01912535f886efca696dd2263a8
Bassel <mail2bassel@yahoo.com>
2020-10-03 18:34:35 -0400

    Installed react-iframe npm package, added iframe

--- src/component/About.js
+++ src/component/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from './Layout'
+import Iframe from 'react-iframe'
 
 const About = () => (
 
@@ -7,6 +8,16 @@ const About = () => (
         <Layout>
             <h2>Welcome to SpaceNearby!</h2>
             <p>This web application allows you to explore nearby space research and development centres, right in your backyard!</p>
+
+            <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
+                    width="450px"
+                    height="450px"
+                    id="myId"
+                    className="myClassname"
+                    display="initial"
+                    position="relative"
+                    frameborder="0"
+                    src="https://www.google.com/maps/embed/v1/search?key=AIzaSyAPHaPH5VuQOqpUdh_9Fd55cduWiybq4qs&q=record+stores+in+Seattle" allowfullscreen></Iframe>/>
         </Layout>
 
     </div>
