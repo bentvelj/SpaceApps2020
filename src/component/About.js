f10d3f866cfe0c9824c86ae9a30c097cca22588a
bentvelj <jaredbentvelsen@gmail.com>
2020-10-03 17:48:50 -0600

    added space to Space_Nearby

--- src/component/About.js
+++ src/component/About.js
@@ -6,7 +6,7 @@ const About = () => (
 
     <div>
         <Layout>
-            <h2>Welcome to SpaceNearby!</h2>
+            <h2>Welcome to Space Nearby!</h2>
             <p>This web application allows you to explore nearby space research and development centres, right in your backyard!</p>
 
             <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
