19540d136d9d8746f7c0f9dc1893952e7eca989f
bentvelj <jaredbentvelsen@gmail.com>
2020-10-04 00:27:08 -0600

    Location Changing enabled

--- src/component/About.js
+++ src/component/About.js
@@ -7,7 +7,9 @@ class About extends React.Component {
         super(props);
         this.state = {
             pic: [],
-            location: {}
+            location: {},
+            currLoc: "Hamilton",
+            input : ""
         }
     }
     componentDidMount() {
@@ -15,20 +17,41 @@ class About extends React.Component {
             /* geolocation is available */
             navigator.geolocation.getCurrentPosition((position) => {
                 this.setState({
-                    location: position.coords
+                    location: position.coords,
+                    currLoc : position.coords.latitude + ',' + position.coords.longitude
+                    //currLoc : "toronto"
                 })
             });
         }
-        console.log(this.state.location);
+        // console.log("Location " + this.state.location.latitude + " " + this.state.location.longitude);
+        // console.log("CURRLOC" + this.state.currLoc);
+    }
+    
+    info_box_style = () => {
+        return {
+            position: "Absolute",
+            top: "50%",
+            right: "50%",
+        }
+    }
+    text_box_attrib = () => {
+        return{
+            autofocus : "false",
+            resize : "none",
+            width : "300px",
+            height : "30px",
+            margin : "5px"
+        }
     }
     render() {
         return (
-            <div>
+            <div>                
+
                 <Layout>
                     <h2>Welcome to Space Nearby!</h2>
                     <p>This web application allows you to explore nearby space research and development centres, right in your backyard!</p>
 
-                    <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
+                    <Iframe src={`https://www.google.com/maps/embed/v1/search?key=AIzaSyAPHaPH5VuQOqpUdh_9Fd55cduWiybq4qs&q=${this.state.currLoc}`}
                         width="450px"
                         height="450px"
                         id="myId"
@@ -36,17 +59,25 @@ class About extends React.Component {
                         display="initial"
                         position="relative"
                         frameborder="0"
-                        src={`https://www.google.com/maps/embed/v1/search?key=AIzaSyAPHaPH5VuQOqpUdh_9Fd55cduWiybq4qs&q=${this.state.location.latitude}, ${this.state.location.longitude}`} allowfullscreen></Iframe>
-                </Layout>
+                        allowfullscreen>   
+                    </Iframe>
+                
 
+                </Layout>
+                <div className="container" style={this.info_box_style()}>
+                    <div>
+                        <h3>Current Location:</h3>
+                        <p>We think you're here: <b>{this.state.currLoc}</b></p>
+                        <input type="text" onChange={(e)=>this.setState({input : e.target.value})} style={this.text_box_attrib()} placeholder="Enter location..."/>
+                        <input type="button" className="btn btn-outline-primary" value="Submit" onClick={()=>this.setState({currLoc : this.state.input})}/>
+                        
+                    </div>
+                </div>
             </div>
+        
         )
     }
 }
-
-
-
-
 
 
 export default About
