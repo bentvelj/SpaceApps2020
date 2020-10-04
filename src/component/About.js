00efc20cc399c42b7b1f67aa68b5cb56334804cb
Christopher Andrade <christopherandrade716@gmail.com>
2020-10-04 05:51:18 -0400

    Adding a bunch of code to About component.

--- src/component/About.js
+++ src/component/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from './Layout'
 import Iframe from 'react-iframe'
+import e from 'express';
 
 class About extends React.Component {
     constructor(props) {
@@ -10,8 +11,7 @@ class About extends React.Component {
             location: {},
             currLoc: "Hamilton",
             input : "Enter Location...",
-            rad_input : 50000,
-            radius : 50000 // 50km
+            rad_input : 50000
         }
     }
     componentDidMount() {
@@ -56,6 +56,57 @@ class About extends React.Component {
             margin : "5px"
         }
     }
+
+    areas_style = () =>
+    {
+        return{
+            position: "absolute",
+            left: "65%",
+            bottom: "10%",
+            width: "550px",
+            height: "400px"
+        }
+    }
+
+    areas_click = () =>
+    {
+
+    }
+
+    get_locations = async () =>
+    {
+        let keywords = "Space%20Agency|Astronomy|Space%20Station|NASA|SpaceX|CSA|Planetarium|Launch%20Site|Outer%20Space";
+        let key = "AIzaSyAPHaPH5VuQOqpUdh_9Fd55cduWiybq4qs";
+        let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + this.state.location.latitude + "," + this.state.location.longitude + "&radius=" + this.state.rad_input + "&keyword=" + keywords + "&key=" + key;
+        let proxy = "https://cors-anywhere.herokuapp.com/";
+        let data;
+        let locations = [];
+
+        await fetch(proxy + url).then(response => response.json()).then(output => { data = output.results});
+
+        for(let index = 0; index < data.length; index++)
+        {
+            let latitude, longitude, name, address;
+            latitude = data[0].geometry.location.lat;
+            longitude = data[0].geometry.location.lng;
+            name = data[0].name;
+            address = data[0].vicinity;
+            locations.push([latitude, longitude, name, address]);
+        }
+
+        console.log(locations);
+
+        let areas = document.getElementById("areas");
+        areas.innerHTML = "";
+
+        for(let index = 0; index < locations.length; index++)
+        {
+            let text = locations[index][2] + ", " + locations[index][3];
+            let element = <li onClick={this.areas_click()}>{text}</li>;
+            
+        }
+    }
+
     render() {
         return (
             <div>                
@@ -85,7 +136,7 @@ class About extends React.Component {
                         <input type="button" className="btn btn-outline-primary" value="Submit" onClick={()=>this.setState({currLoc : this.state.input})}/>
                     </div>
                     <div>
-                        <input type="button" class="btn btn-outline-success" value="Back to my Location" onClick={function(){
+                        <input type="button" className="btn btn-outline-success" value="Back to my Location" onClick={function(){
                             this.setState({currLoc : `${this.state.location.latitude},${this.state.location.longitude}`})
                             document.getElementById("input-text").value = "";
                         }.bind(this)}/>
@@ -95,11 +146,24 @@ class About extends React.Component {
                         <h3>Current Radius:</h3>
                         <p><b>{this.state.radius} meter(s)</b></p>
                         <input type="text" onChange={(e)=>this.setState({rad_input : e.target.value})} id="input-text" style={this.text_box_attrib()} placeholder={"Enter Radius..."}/>
-                        <input type="button" className="btn btn-outline-danger" value="Submit" onClick={function(){
+                        <input type="button" className="btn btn-outline-danger" value="Submit" onClick={function()
+                        {
                             if(this.state.rad_input <= 1 || isNaN(this.state.rad_input)) return;
                             this.setState({radius : Math.floor(this.state.rad_input)})
-                            }.bind(this)}/>
-                    </div>
+
+                            this.get_locations();
+
+                            console.log(document.getElementById("areas"));
+
+                        }.bind(this)}/>
+                </div>
+
+                <div style={this.areas_style()}>
+                    <nav>
+                        <ul id="areas">
+                        </ul>
+                    </nav>
+                </div>
             </div>
         
         )
